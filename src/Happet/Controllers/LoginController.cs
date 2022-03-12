dbe3dd89f20ad3faa466fc15306f3e2901fcfa70
Lucas Alves <lucas.olvalves@gmail.com>
2022-03-12 19:08:48 -0300

    Finalize login screen

---
+++ src/Happet/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Happet.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
